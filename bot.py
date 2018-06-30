bf599d6d7aef8321c11b5bc2331613a9384c95f9
korni <dkostovasilis@yahoo.com>
2018-06-30 23:01:23 +0100

    Changes to allow LFG commands at bot_commands channel

--- bot.py
+++ bot.py
@@ -30,6 +30,12 @@ del_commands = [
     "no_game"
 ]
 
+# LFG related commands can only be called in the channels below
+lfgChannels = [
+        "game_request",
+        "bot_commands"
+        ]
+
 minutes_in_a_day = 1440
 expiration_times = {}
 role = 0
@@ -83,11 +89,9 @@ async def kj_facepalm(ctx):
     await ctx.send(embed=embed)
 
 
-@bot.command(pass_context=True,aliases=["lfg","game"])
+@bot.command(pass_context=True, aliases=["lfg", "game"])
 async def LFG(ctx, minutes=minutes_in_a_day):
-    if not str(ctx.message.channel) == "game_request":
-        await ctx.send("Please " + ctx.message.author.mention + ", use the appropriate channel for this command.")
-    else:
+    if any(item == str(ctx.message.channel) for item in lfgChannels):
         if role in ctx.message.author.roles:
             await ctx.message.author.remove_roles(role)
             await ctx.send(str(ctx.message.author.name) + " is no longer looking for a game.")
@@ -96,23 +100,22 @@ async def LFG(ctx, minutes=minutes_in_a_day):
             expiration_times[ctx.author.id] = expiration_time
             await ctx.message.author.add_roles(role)
             await ctx.send("Hey, <@&" + str(id["LFG"]) + ">! " + str(ctx.message.author.name) + " is looking for a game.")
-
+    else:
+        await ctx.send("Please " + ctx.message.author.mention + ", use the appropriate channels for this command.")
 
 @bot.command(pass_context=True, aliases=["no_lfg", "no_game", "remove_lfg"])
 async def no_LFG(ctx, minutes=minutes_in_a_day):
-    if not str(ctx.message.channel) == "game_request":
-        await ctx.send("Please " + ctx.message.author.mention + ", use the appropriate channel for this command.")
-    else:
+    if any(item == str(ctx.message.channel) for item in lfgChannels):
         if role in ctx.message.author.roles:
             await ctx.message.author.remove_roles(role)
             await ctx.send(str(ctx.message.author.name) + " is no longer looking for a game.")
+    else:
+        await ctx.send("Please " + ctx.message.author.mention + ", use the appropriate channels for this command.")
 
 
 @bot.command(pass_context=True, aliases=["whos_lfg"])
 async def whos_LFG(ctx):
-    if not str(ctx.message.channel) == "game_request":
-        await ctx.send("Please " + ctx.message.author.mention + ", use the appropriate channel for this command.")
-    else:
+    if any(item == str(ctx.message.channel) for item in lfgChannels):
         currently_looking = []
         role = discord.utils.get(ctx.message.guild.roles, name="LFG")
         for member in ctx.message.guild.members:
@@ -125,6 +128,8 @@ async def whos_LFG(ctx):
             await ctx.send(ctx.message.author.mention + ": The following users are looking for a game:\n" + ', '.join([str(x.name) for x in currently_looking[:-1]])+ " and " + currently_looking[-1].name)
         else:
             await ctx.send(ctx.message.author.mention + ": Nobody is looking for a game. :(")
+    else:
+        await ctx.send("Please " + ctx.message.author.mention + ", use the appropriate channels for this command.")        
 
 
 @bot.command(pass_context=True)
@@ -138,12 +143,10 @@ bot.remove_command('help')
 @bot.command(pass_context=True)
 async def help(ctx):
     embed = discord.Embed(title="Looking For Game Bot", description="Keeps track of who is currently looking for a game. The following commands are available:", color=0xeee657)
-
     embed.add_field(name="!LFG [minutes]", value="Toggles your role for LFG. You can limit the length of time you will be LFG by entering a number of minutes after the command.", inline=False)
     embed.add_field(name="!whos_LFG", value="Tells you who is currently looking.", inline=False)
     embed.add_field(name="!info", value="Gives a little info about the bot.", inline=False)
     embed.add_field(name="!help", value="Gives this message.", inline=False)
-
     await ctx.send(embed=embed)
 
 
@@ -154,7 +157,6 @@ async def check_LFG():
         for uid, expiration_time in expiration_times.items():
             if datetime.now() > expiration_time:
                 await discord.utils.get(bot.get_all_members(), id=uid).remove_roles(role)
-
         await asyncio.sleep(60)
 
 bot.loop.create_task(check_LFG())
