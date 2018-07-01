7895e30aa51b906f3be490aaaf54b0854c9171b7
korni <dkostovasilis@yahoo.com>
2018-07-01 12:27:04 +0100

    Refreshed command names

--- bot.py
+++ bot.py
@@ -28,7 +28,11 @@ del_commands = [
     "LFG",
     "no_LFG",
     "no_lfg",
-    "no_game"
+    "no_game",
+    "go",
+    "nogo",
+    "GO",
+    "NOGO"
 ]
 
 # LFG related commands can only be called in the channels below
@@ -90,8 +94,8 @@ async def kj_facepalm(ctx):
     await ctx.send(embed=embed)
 
 
-@bot.command(pass_context=True, aliases=["lfg", "game", "go", "play"])
-async def LFG(ctx, minutes=minutes_in_a_day):
+@bot.command(pass_context=True, aliases=["lfg", "game", "LFG", "play", "GO", "PLAY", "GAME"])
+async def go(ctx, minutes=minutes_in_a_day):
     if any(item == str(ctx.message.channel) for item in lfgChannels):
         if role in ctx.message.author.roles:
             await ctx.message.author.remove_roles(role)
@@ -104,8 +108,9 @@ async def LFG(ctx, minutes=minutes_in_a_day):
     else:
         await ctx.send("Please " + ctx.message.author.mention + ", use the appropriate channels for this command.")
 
-@bot.command(pass_context=True, aliases=["no_lfg", "no_game", "remove_lfg"])
-async def no_LFG(ctx, minutes=minutes_in_a_day):
+
+@bot.command(pass_context=True, aliases=["no_lfg", "no_LFG" "no_game", "remove_lfg"])
+async def nogo(ctx):
     if any(item == str(ctx.message.channel) for item in lfgChannels):
         if role in ctx.message.author.roles:
             await ctx.message.author.remove_roles(role)
@@ -159,8 +164,9 @@ bot.remove_command('help')
 
 @bot.command(pass_context=True)
 async def help(ctx):
-    embed = discord.Embed(title="Looking For Game Bot", description="Keeps track of who is currently looking for a game. The following commands are available:", color=0xeee657)
-    embed.add_field(name="!LFG [minutes]", value="Toggles your role for LFG. You can limit the length of time you will be LFG by entering a number of minutes after the command.", inline=False)
+    embed = discord.Embed(title="Looking For Game (LFG) Bot", description="Keeps track of who is currently looking for a game. The following commands are available:", color=0xeee657)
+    embed.add_field(name="!go [minutes]", value="Toggles your role for LFG. You can limit the length of time you will be LFG by entering a number of minutes after the command.", inline=False)
+    embed.add_field(name="!nogo", value="Removes the user from the LFG group.", inline=False)
     embed.add_field(name="!whos_LFG", value="Tells you who is currently looking.", inline=False)
     embed.add_field(name="!info", value="Gives a little info about the bot.", inline=False)
     embed.add_field(name="!help", value="Gives this message.", inline=False)
@@ -174,6 +180,7 @@ async def check_LFG():
         for uid, expiration_time in expiration_times.items():
             if datetime.now() > expiration_time:
                 await discord.utils.get(bot.get_all_members(), id=uid).remove_roles(role)
+
         await asyncio.sleep(60)
 
 bot.loop.create_task(check_LFG())
